e5b93bb638db766a7165ea695928cc3345912016
Kristie-Lynne Koss <kaayell@gmail.com>
2016-11-29 17:54:03 -0600

    initial commit

---
+++ src/main/java/com/chicago/labs/CoffeeTalksApplication.java
@@ -0,0 +1,12 @@
+package com.chicago.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeTalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeTalksApplication.class, args);
+	}
+}
